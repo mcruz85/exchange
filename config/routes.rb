5f20f7b2ab1ef2ffb31ef01fdba1ee71baf463d9
Marcus Cruz <marcus.cruz85@gmail.com>
2018-01-11 00:38:54 -0300

    remane images

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+
+  post '/exchange', to: 'home#exchange'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
