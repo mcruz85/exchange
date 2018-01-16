393b50d136aea8626b16eadba81953a1f260c7b1
Marcus Cruz <marcus.cruz85@gmail.com>
2018-01-15 23:10:48 -0300

    created exchange_box coffee script

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+
+  root to: 'home#index'
+
   get 'home/index'
-
-
   post '/exchange', to: 'home#exchange'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
