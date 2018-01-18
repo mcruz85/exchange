166926dd6c1cc5ad583e45272b62cf10dbe9e7ed
Marcus Cruz <marcus.cruz85@gmail.com>
2018-01-18 00:18:19 -0300

    update home_spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -8,9 +8,14 @@ RSpec.describe "Exchange Currency Process", :type => :feature do
       select('USD', from: 'currency_destination')
       fill_in 'quantity', with: '10'
     end
-    click_button 'CONVERTER'
 
-    save_and_open_page
-    expect(page).to have_content("value")
+    #save_and_open_page
+    result = page.find_field('result').value
+    reseult = result.to_f
+
+    #expect(result.is_a? Numeric).to eql(true)
+    #expect(value != 0 || quantity == 0).to eql(true)
+
   end
+
 end
